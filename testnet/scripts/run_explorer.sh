21e456585c6887275f5006b81c56f9cf3e943956
gusto <bacvinka@gmail.com>
2026-02-13 12:05:03 +0200

    Explorer version with relative resource path

--- testnet/scripts/run_explorer.sh
+++ testnet/scripts/run_explorer.sh
@@ -8,4 +8,4 @@ export NBE_NODE_MANAGER="${NBE_NODE_MANAGER:-noop}"
 export NBE_NODE_API_HOST="${NBE_NODE_API_HOST:-127.0.0.1}"
 export NBE_NODE_API_PORT="${NBE_NODE_API_PORT:-8080}"
 
-exec /opt/explorer/.venv/bin/python /opt/explorer/src/main.py
+exec /opt/explorer/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers
