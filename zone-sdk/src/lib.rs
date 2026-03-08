e425fa13cc43e105325cb2e384df0eb54323a366
gusto <bacvinka@gmail.com>
2026-03-08 18:47:18 +0200

    wip: export

--- zone-sdk/src/lib.rs
+++ zone-sdk/src/lib.rs
@@ -4,6 +4,14 @@ pub mod state;
 
 use lb_core::mantle::ops::channel::MsgId;
 
+// Reexports
+pub use lb_chain_broadcast_service::BlockInfo;
+pub use lb_chain_service as chain;
+pub use lb_common_http_client as http;
+pub use lb_common_http_client::{BasicAuthCredentials, CommonHttpClient, Error as HttpError};
+pub use lb_core::{block, header, mantle};
+pub use lb_key_management_system_service as kms;
+
 /// A zone block — opaque data published to / read from a channel.
 pub struct ZoneBlock {
     /// The unique identifier of this inscription.
