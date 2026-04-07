f883fec4cfa1b5bfcfc451e7b8b9b84f6634b708
Petar Radovic <petar.radovic@gmail.com>
2026-04-07 16:07:05 +0200

    fix(ledger): set storage gas price to 0 in genesis

--- ledger/src/cryptarchia/mod.rs
+++ ledger/src/cryptarchia/mod.rs
@@ -599,7 +599,7 @@ impl LedgerState {
             average_execution_gas: 0.into(),
             execution_base_fee: 0.into(),
             storage_gas_ema: 0.into(),
-            storage_gas_price: 1.into(),
+            storage_gas_price: 0.into(),
             storage_gas_consumed_in_epoch: 0.into(),
         }
     }
